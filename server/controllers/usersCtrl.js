884c69860c6f620cdbfda103c89a176ba5d033fa
Christopher Wilson <cmw.wilson88@gmail.com>
2017-11-28 12:23:38 -0500

    Add server side validations and display errors on client

--- server/controllers/usersCtrl.js
+++ server/controllers/usersCtrl.js
@@ -1,23 +1,32 @@
 import bcrypt from 'bcrypt';
 import isEmpty from 'lodash/isEmpty';
 import knex from '../../db/knex';
+import validateInput from '../../src/shared/validations/signup';
+
+
 
 export function createNewUser(req, res) {
-  const {username, password, email} = req.body;
-  const password_digest = bcrypt.hashSync(password, 10);
+  // const {username, password, email} = req.body;
+  // const password_digest = bcrypt.hashSync(password, 10);
 
-  knex('users').insert({
-    username,
-    email,
-    password_digest
-  }).then(() => {
-    knex.select().from('users')
-      .then(users => {
-        res.status(200).json(users)
-      })
-  }).catch(err => {
-      res.status(500).json(err)
-  })
+  // knex('users').insert({
+  //   username,
+  //   email,
+  //   password_digest
+  // }).then(() => {
+  //   knex.select().from('users')
+  //     .then(users => {
+  //       res.status(200).json(users)
+  //     })
+  // }).catch(err => {
+  //     res.status(500).json(err)
+  // })
+
+  const {errors, isValid} = validateInput(req.body);
+
+  if(!isValid) {
+    res.status(400).json(errors);
+  }
 }
 
 export function checkIfUserExists(req, res) {
--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const PORT = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors());
 
 app.use('/api/users', users);
 app.use('/api/auth', auth);
