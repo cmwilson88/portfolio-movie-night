35e01aee93b64057b2432df1f535b2d22b9b02c7
Christopher Wilson <cmw.wilson88@gmail.com>
2017-11-28 14:37:38 -0500

    Check uniqueness on server side

--- server/controllers/usersCtrl.js
+++ server/controllers/usersCtrl.js
@@ -1,34 +1,56 @@
 import bcrypt from 'bcrypt';
 import isEmpty from 'lodash/isEmpty';
 import knex from '../../db/knex';
-import validateInput from '../../src/shared/validations/signup';
+import commonValidations from '../../src/shared/validations/signup';
 
+
+function validateInput(data, otherValidations) {
+  let { errors } = otherValidations(data);
+
+  return knex('users')
+          .where('username', data.username)
+          .orWhere('email', data.email)
+            .then(user => {
+              user = user[0]
+              if(user) {
+                if(user.username === data.username) {
+                  errors.username = 'There is user with such username'
+                }
+                if(user.email === data.email) {
+                  errors.email = 'There is user with such email'
+                }
+              }
+
+              return {
+                errors,
+                isValid: isEmpty(errors)
+              }
+            })
+}
 
 
 export function createNewUser(req, res) {
-  // const {username, password, email} = req.body;
-  // const password_digest = bcrypt.hashSync(password, 10);
+  
+  validateInput(req.body, commonValidations).then(({ errors, isValid}) => {
+    if(isValid) {
+      const {username, password, email} = req.body;
+      const password_digest = bcrypt.hashSync(password, 10);
+    
+      knex('users').insert({
+        username,
+        email,
+        password_digest
+      }).then(user => {
+          res.status(200).json({ success: true })
+      }).catch(err => {
+          res.status(500).json({ error: err })
+      })
+    } else {
+      res.status(400).json(errors);
+    }
+    
+  });
 
-  // knex('users').insert({
-  //   username,
-  //   email,
-  //   password_digest
-  // }).then(() => {
-  //   knex.select().from('users')
-  //     .then(users => {
-  //       res.status(200).json(users)
-  //     })
-  // }).catch(err => {
-  //     res.status(500).json(err)
-  // })
-
-  const {errors, isValid} = validateInput(req.body);
-
-  if(isValid) {
-    res.status(200).json({ success: true })
-  } else {
-    res.status(400).json(errors);
-  }
 }
 
 export function checkIfUserExists(req, res) {
