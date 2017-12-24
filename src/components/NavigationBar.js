d2e97d1b96cb9dd114216811fd9d615e8b1310d3
Christopher Wilson <cmw.wilson88@gmail.com>
2017-12-23 23:26:58 -0500

    Hook up open modal method to navigation bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { logout } from 'redux/actions/auth-actions';
+import { openModal, hideModal } from 'redux/actions/modal-actions'
 import { addFlashMessage } from 'redux/actions/flash-actions';
 
 class NavigationBar extends React.Component {
@@ -71,6 +72,7 @@ class NavigationBar extends React.Component {
 
     render() {
         const { isAuthenticated } = this.props.auth
+        const {openModal} = this.props;
         const navClasses = classnames(
             'navigation',
             {
@@ -80,7 +82,7 @@ class NavigationBar extends React.Component {
         const guestLinks = (
             <ul className="navigation__buttons-container">
                 <li>
-                    <Link className="btn btn--small btn--white" to="/signup">Sign In</Link>
+                    <div className="btn btn--small btn--white" onClick={() => openModal('AUTH')}>Sign In</div>
                 </li>
             </ul>
         )
@@ -110,7 +112,9 @@ class NavigationBar extends React.Component {
 NavigationBar.propTypes = {
     auth: PropTypes.object.isRequired,
     logout: PropTypes.func.isRequired,
-    addFlashMessage: PropTypes.func.isRequired
+    addFlashMessage: PropTypes.func.isRequired,
+    openModal: PropTypes.func,
+    hideModal: PropTypes.func
 }
 
 function mapStateToProps(state) {
@@ -119,4 +123,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps, { logout, addFlashMessage })(NavigationBar);
+export default connect(mapStateToProps, { logout, addFlashMessage, openModal, hideModal })(NavigationBar);
